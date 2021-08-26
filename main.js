524d2c06ee872354b82266378266066e985bede4
fathur-k <fathurkaryim@gmail.com>
2021-08-26 16:05:44 +0700

    generate id

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 let input = []
+
+let kodePoli = {
+  'poli anak': 'PA',
+  'poli gigi': 'PG',
+  'poli internis': 'PI',
+  'poli bedah': 'PB',
+  'poli umum': 'PU'
+}
+//ID berisi inisial nama, tanggal, dan nama poli. (ex. anak)
+function generateId(nama, tgl, poli) {
+  let id = `${nama[0].toUpperCase() + tgl + kodePoli[poli]}`
+  return id
+}
+console.log(generateId('fathur', 10 , 'poli anak'))
