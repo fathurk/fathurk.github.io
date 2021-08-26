2ab045be6000f124363c30f8ccfae4f62d59f9cd
fathur-k <fathurkaryim@gmail.com>
2021-08-26 18:28:31 +0700

    database

--- main.js
+++ main.js
@@ -1,108 +1,28 @@
-
-// let input = []
-// //jika sabtu minggu yang enable poliUmum
-
-function infoAppointment(user) {
-    // const dataBase = [
-    //     {
-    //     namePoli: 'poli anak', 
-    //     dokter1: {
-    //         name: 'Raihan',
-    //         avaliable: ['senin','rabu','kamis']
-    //         },
-    //     dokter2: {
-    //         name: 'Matthew',
-    //         avaliable: ['selasa','kamis','jumat']
-    //         },
-    //     dokter3: {
-    //         name: 'fathur',
-    //         avaliable: ['senin','selasa','jumat']
-    //         }
-    //     },
-    //     {
-    //     namePoli: 'poli internis', 
-    //     dokter1: {
-    //         name: 'Agnia',
-    //         avaliable: ['selasa','rabu','kamis']
-    //         },
-    //     dokter2: {
-    //         name: 'Arif',
-    //         avaliable: ['selasa','kamis','jumat']
-    //         },
-    //     dokter3: {
-    //         name: 'Colin',
-    //         avaliable: ['senin','rabu','kamis']
-    //         }
-    //     },
-    //     {
-    //     namePoli: 'poli gigi', 
-    //     dokter1: {
-    //         name: 'Darwin',
-    //         avaliable: ['selasa','rabu','kamis']
-    //         },
-    //     dokter2: {
-    //         name: 'Yura',
-    //         avaliable: ['senin','kamis','jumat']
-    //         },
-    //     dokter3: {
-    //         name: 'Huang',
-    //         avaliable: ['senin','rabu','jumat']
-    //         }
-    //     },
-    //     {
-    //     namePoli: 'poli umum', 
-    //     dokter1: {
-    //         name: 'Agnia',
-    //         avaliable: ['senin','kamis','jumat']
-    //         },
-    //     dokter2: {
-    //         name: 'Arif',
-    //         avaliable: ['selasa','rabu','jumat']
-    //         },
-    //     dokter3: {
-    //         name: 'Colin',
-    //         avaliable: ['senin','kamis','rabu']
-    //         }
-    //     },
-    //     {
-        // namePoli: 'poli bedah', 
-        // dokter1: {
-        //     name: 'Agnia',
-        //     avaliable: ['selasa','rabu','kamis']
-        //     },
-        // dokter2: {
-        //     name: 'Arif',
-        //     avaliable: ['senin','selasa','jumat']
-        //     },
-        // dokter3: {
-        //     name: 'Juluis',
-        //     avaliable: ['senin','rabu','jumat']
-        //     }
-    //     }  
-    // ]
-
-    const dataBase = [
-        namePoli:'poli anak', dokter : {attack: 890, defense: 700, agility: 800, magic: 870}}
+// //jika sabtu minggu yang enable poliUmum 
+// 09 s/d 18 diluar itu  jam istirahat atau di luar jam kerja
+function infoAppointment(user) {     
+    const dataBase = [         
+        {namePoli: 'poli anak', dokter: ['Raihan','Matthew','fathur']},         {namePoli: 'poli internis', dokter: ['Agnia','Arif','Colin']},
+        {namePoli: 'poli gigi', dokter: ['Darwin','Yura','Huang']},
+        {namePoli: 'poli umum', dokter:['Gilbert','Krisna','Juluis']},         {namePoli: 'poli bedah', dokter:['Indra','Vina','Royan']}
     ]
-    let output = []
-    for ( let i = 0; i < dataBase.length; i++) {
-        console.log(dataBase[i].dokter1);
-    }
-
-}
-
+    let avaliable = ['senin','selsa','rabu','kamis','jumat']              
+    let output = []     
+    for (let i = 0; i < dataBase.length; i++) {         
+        console.log(dataBase[i].dokter);
+    }  
+}  
 let pengunjung = {
-    naame: 'Surya',
-    tglLaahir: '18/06/1996',
-    gender: 'male',
-    poli: 'poli gigi',
-    waktuAppointment: '14.30',
-    keluhan: 'gusi bengkak',
-    dateBooking: ['selasa', '31/08/2021']
-
-}
-
+    name: 'Surya',     
+    tglLaahir: '18/06/1996',     
+    gender: 'male',     
+    poli: 'poli gigi',     
+    keluhan: 'gusi bengkak',     
+    dateBooking: ['minggu', '31/08/2021']  
+}  
 console.log(infoAppointment(pengunjung));
+
+
 
 let input = []
 
